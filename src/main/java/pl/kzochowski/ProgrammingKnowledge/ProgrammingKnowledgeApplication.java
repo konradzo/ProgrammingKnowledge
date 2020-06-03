3efbfddfb554d3bf7bdcadd92b3214888deabd3b
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2020-06-03 22:23:07 +0200

    initial commit

---
+++ src/main/java/pl/kzochowski/ProgrammingKnowledge/ProgrammingKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package pl.kzochowski.ProgrammingKnowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingKnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingKnowledgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kzochowski/ProgrammingKnowledge/ProgrammingKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kzochowski.ProgrammingKnowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
